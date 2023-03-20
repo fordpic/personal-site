c4f5eb0fff4160b53d347c62399d049adfb28c8e
Ford Pickert <fordpickert@gmail.com>
2023-03-20 17:19:57 -0500

    landing page gen styline done, no pallette

--- src/pages/components/Content.tsx
+++ src/pages/components/Content.tsx
@@ -5,39 +5,56 @@ export default function Content() {
 
 	return (
 		<div className='flex flex-col text-center text-lg justify-evenly min-h-screen tracking-wide border-2 border-pink-500'>
-			<p className='p-20 bg-pink-200 text-xl font-bold'>
-				Hi there! My name is{' '}
-				<span className='text-red-500 font-bold'>Ford Pickert</span> and I am a
-				full-stack sofware engineer!
+			<p className='p-12 text-xl font-bold'>
+				My name is <span className='text-red-500 font-bold'>Ford Pickert</span>{' '}
+				and I am a full-stack sofware engineer!
 			</p>
 
-			<p className='p-8 px-20 bg-pink-200'>
-				I have been working in tech as a software engineer for the past two
-				years now, focusing primarily on improving my skills with{' '}
+			<p className='p-12 px-24 bg-pink-400'>
+				I have been working as a software engineer for the past two years now,
+				focusing primarily on improving my skills with{' '}
 				<span className='text-blue-500 font-bold'>TypeScript</span> and{' '}
 				<span className='text-blue-500 font-bold'>Python</span> as well as
 				learning new tools and libraries that I find interesting.
 				<br />
 				<br />
 				Ideally, I would love to find a job where I am constantly challenged and
-				can improve my skills daily.
+				can improve my craft daily. I enjoy tackling new problems and would look
+				to assist team members wherever needed; if that means learning an
+				entirely new stack if it will benefit the team, then so be it!
 				<br />
 				<button
 					onClick={() => router.push(`/Projects`)}
-					className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-4 justify-center'>
+					className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-6 justify-center'>
 					See All Projects
 				</button>
 			</p>
 
-			<p className='p-20 bg-pink-200'>
+			<p className='p-12 px-24 bg-pink-200'>
+				When I am not coding, I greatly enjoy reading and writing, which are two
+				things that I have done extensively for my entire life. I have read tons
+				of books ranging from classical history to modern spy thrillers and
+				everything in between. I keep a reading list of my favorite non-fiction
+				literature around to reference for my friends when they ask for a
+				recommendation; please feel free to take a look at the list below if
+				interested!
+				<br />
+				<button
+					onClick={() => router.push(`/Books`)}
+					className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-6 justify-center'>
+					See Reading List
+				</button>
+			</p>
+
+			<p className='p-12 px-24 bg-pink-400'>
 				Below you can find my resume, GitHub profile as well as numerous ways of
 				reaching me - please reach out if you would like to discuss my past, my
 				future, or any potential job openings that may be the right fit!
 				<br />
-				<button className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-4 mr-1 justify-center'>
+				<button className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-6 mr-1 justify-center'>
 					See Resume
 				</button>
-				<button className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-4 ml-1 justify-center'>
+				<button className='border border-red-500 bg-red-500 hover:bg-red-300 rounded-xl h-16 w-48 mt-6 ml-1 justify-center'>
 					See GitHub
 				</button>
 			</p>
--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
 	return (
-		<header className='border-2 border-green-500 mx-auto min-w-full py-4 text-center'>
-			<h1 className='font-extrabold tracking-widest text-3xl'>FORDS PAGE</h1>
-		</header>
+		<div className='border-2 border-green-500 mx-auto min-w-full py-4 text-center bg-[url("../../public/static.avif")]'>
+			<h1 className='font-extrabold tracking-widest text-4xl text-red-800'>
+				FORDS PAGE
+			</h1>
+		</div>
 	);
 }
