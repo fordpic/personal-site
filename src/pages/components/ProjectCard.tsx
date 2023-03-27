7743627e19da8baaf79e7875fc035d7e00e39298
Ford Pickert <fordpickert@gmail.com>
2023-03-27 15:08:39 -0500

    small style clean before mvp

--- src/pages/components/ProjectCard.tsx
+++ src/pages/components/ProjectCard.tsx
@@ -3,7 +3,7 @@ import Bubble from './Bubble';
 export default function ProjectCard({ project }: any) {
 	return (
 		<div className='text-white flex flex-col space-y-8 border-b border-red-400 mx-24 p-8'>
-			<h1 className='text-red-500 text-xl font-bold'>{project.title}</h1>
+			<h1 className='text-red-500 text-2xl font-bold'>{project.title}</h1>
 			{/* <img /> */}
 			<div className='flex justify-center space-x-4'>
 				{project.tech.map((proj: any) => (
